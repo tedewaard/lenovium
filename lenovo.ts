ee3d6259ec960581978a840cfa1a029545162a46
Trevor <tr.edewaard@gmail.com>
2023-12-20 21:43:57 +0000

    Get warranty data for a test endpoint

---
+++ lenovo.ts
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+
+const TOKEN = process.env.TOKEN
+const TARGET = 'https://supportapi.lenovo.com/v2.5/warranty?Serial=PF42ZLHB'
+
+var myHeaders = new Headers();
+myHeaders.append("ClientID", TOKEN as string);
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+}
+
+const response = await fetch(TARGET, requestOptions); 
+
+let data = await response.json();
+console.log(data);
